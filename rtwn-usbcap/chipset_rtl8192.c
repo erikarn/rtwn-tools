5c386caab0a5f5b1777cd5a520b3b13f7211b5c5
Adrian Chadd <adrian@FreeBSD.org>
2024-12-01 18:30:33 -0800

    rtwn-usbpcap: add rtl8192 specific rx alignment
    now I need to make them selectable at runtime

--- rtwn-usbcap/chipset_rtl8192.c
+++ rtwn-usbcap/chipset_rtl8192.c
@@ -30,7 +30,25 @@
 
 #include "if_rtwn_desc.h"
 
+#include "main.h"
+
+static int
+chipset_rtl8192_rx_align(rtwn_app_t *ra, int pkt_len, int usb_frame_len)
+{
+
+	return roundup2(pkt_len, 128);
+}
+
 /*
- * RTL8192 specific things
+ * RTL8188/RTL8192 specific things
  */
 
+static struct usb_chipset_ops chipset_rtl8192_ops = {
+	.rx_align = chipset_rtl8192_rx_align,
+};
+
+void
+chipset_rtl8192_init(rtwn_app_t *ra)
+{
+	ra->ops = &chipset_rtl8192_ops;
+}
